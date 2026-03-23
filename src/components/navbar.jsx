961a1770ee13f7dd68b7182a8ba6bc50c3ce7462
Bibby850 <larandrade116@gmail.com>
2026-03-23 11:06:28 +0000

    build: navbar; perf: icons
    improved the icons functions, created "states" for the navbar, still not finished

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import "../styles/variable.css";
 import "../styles/navbar.css";
 import Button from "./button.jsx";
-import {Logo} from "./images.jsx";
+import { Logo } from "./images.jsx";
 
 export default function Navbar() {
+  var [user, setUser] = useState(null); //TODO substituir
   return (
     <nav>
       <div className="container-fluid">
@@ -11,9 +13,32 @@ export default function Navbar() {
           <div className="col-1">
             <Logo />
           </div>
-          <div className="col-9 text-start"><Button link="/" text = "Home" /></div>
-          <div className="col-1 text-end"><Button link="/" text = "Sign In" /></div>
-          <div className="col-1 text-start"><Button link="/" text = "Sign Up" /></div>
+          <div className="col-9 text-start">
+            <Button link="/" text="Home" />
+          </div>
+          {user ? (
+            <>
+              <div className="col-1 text-end">
+                <Button link="/" text={user.name} />
+              </div>
+            {user.picture ? ( <> <div className="col-1 text-start">
+                <Button link="/" text="Sign Out" />
+              </div></>
+              
+            ): (<><div className="col-1 text-start">
+                <Button link="/" text="Sign Up" />
+              </div></>)}
+            </>
+          ) : (
+            <>
+              <div className="col-1 text-end">
+                <Button link="/" text="Sign In" />
+              </div>
+              <div className="col-1 text-start">
+                <Button link="/" text="Sign Up" />
+              </div>
+            </>
+          )}
         </div>
       </div>
     </nav>
