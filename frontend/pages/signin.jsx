d7b920e093494978594f2e2701aa4818631ca3b1
Bibby850 <larandrade116@gmail.com>
2026-03-25 18:44:56 +0000

    build: signin page
    finished the form only, still has no validation

--- frontend/pages/signin.jsx
+++ frontend/pages/signin.jsx
@@ -1,5 +1,6 @@
 import Navbar from "../components/navbar.jsx";
 import { Image } from "../components/images.jsx";
+import { SigninForm } from "../components/form.jsx";
 
 import "../styles/variable.css";
 import "../styles/signin.css";
@@ -10,8 +11,8 @@ export default function Signup() {
       <Navbar />
       <div className="container-fluid signin-container">
         <div className="row signin-row">
-          <div className="col-6">
-            
+          <div className="col-6 signin-content-col">
+            <SigninForm />
           </div>
           <div className="col-6 signin-image-col">
             <Image name="signBackground" className={"signin-background-wrapper"} />
