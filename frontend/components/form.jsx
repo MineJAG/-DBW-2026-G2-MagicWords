f76dab206fb943f884571c2032ab3cb9b3ede6bb
Bibby850 <larandrade116@gmail.com>
2026-03-28 11:07:55 +0000

    build: made divs for errors

--- frontend/components/form.jsx
+++ frontend/components/form.jsx
@@ -7,14 +7,17 @@ export function SigninForm() {
       <form action="/" method="post" className="row g-3 ">
         <p>Insert your username.</p>
         <input type="text" id="username" placeholder="e.g. klaud23" />
-        <div className="error">*That username is already in use</div>
+        <div className="error"></div>
         <p>Insert your email.</p>
         <input type="email" id="email" placeholder="example@email.com" />
+        <div className="error"></div>
         <p>Insert your password.</p>
         <span>Must be 6–12 characters and include at least one number.</span>
         <input type="password" id="password" placeholder="Password" />
+        <div className="error"></div>
         <p>Confirm password.</p>
         <input type="password" id="passwordVerification" placeholder="Password Confirmation" />
+        <div className="error"></div>
         <input type="submit" id="submit" value="Sign In" />
       </form>
     </div>
