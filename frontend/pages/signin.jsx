f2b35207e55acec034996307ef6097ac1cb4517b
Bibby850 <larandrade116@gmail.com>
2026-03-30 19:29:18 +0100

    build: signin
    done for every screen

--- frontend/pages/signin.jsx
+++ frontend/pages/signin.jsx
@@ -14,16 +14,13 @@ export default function Signin() {
     <div className="signin-page">
       <Navbar />
       <div className="container-fluid signin-container">
-        <div className="row signin-row">
-          <div className="col-6 signin-content-col">
+        <div className="signin-row">
+          <div className="col-12 col-lg-6 signin-content-col">
             <SigninForm />
           </div>
-          <div className="col-6 signin-image-col">
-            <Image name="signBackground" className={"signin-background-wrapper"} />
-          </div>
+          <div className="col-6 signin-image-col"></div>
         </div>
       </div>
     </div>
-
   );
 }
