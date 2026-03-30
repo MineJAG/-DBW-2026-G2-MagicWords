68a0abe3abaf9cf528d6eddd5ddb132a7a5af1e4
Bibby850 <larandrade116@gmail.com>
2026-03-30 10:37:45 +0100

    fix: corrected a typo, build: mobile signin version
    the mobile signin version still isnt complete

--- frontend/hooks/formValidation.js
+++ frontend/hooks/formValidation.js
@@ -1,7 +1,6 @@
 "use strict";
 import { createArrayById } from "../utils/arrayCreation";
 
-
 export function initializeFormValidation() {
   const fieldIds = ["username", "email", "password", "passwordVerification"];
   const fields = createArrayById(fieldIds);
--- frontend/pages/signin.jsx
+++ frontend/pages/signin.jsx
@@ -14,16 +14,23 @@ export default function Signin() {
     <div className="signin-page">
       <Navbar />
       <div className="container-fluid signin-container">
-        <div className="row signin-row">
-          <div className="col-6 signin-content-col">
-            <SigninForm />
-          </div>
-          <div className="col-6 signin-image-col">
-            <Image name="signBackground" className={"signin-background-wrapper"} />
+        {/* Mobile view */}
+        <div className="signin-mobile">
+          <SigninForm />
+        </div>
+
+        {/* Desktop view */}
+        <div className="signin-desktop">
+          <div className="row signin-row">
+            <div className="col-6 signin-content-col">
+              <SigninForm />
+            </div>
+            <div className="col-6 signin-image-col">
+              <Image name="signBackground" className={"signin-background-wrapper"} />
+            </div>
           </div>
         </div>
       </div>
     </div>
-
   );
 }
