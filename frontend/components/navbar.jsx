9507d8568c75a141345eb2fe0663e99d2e91d3c3
Bibby850 <larandrade116@gmail.com>
2026-03-30 17:21:40 +0100

    build: navbar
    navbar is ready for all screens

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Icon } from "./icons.jsx";
 import Button from "./button.jsx";
 import { Image } from "./images.jsx";
@@ -7,13 +7,6 @@ import { Image } from "./images.jsx";
 import "../styles/variable.css";
 import "../styles/navbar.css";
 
-/*  useEffect(() => {
-    setUser({
-      name: null,
-      picture: null,
-    });
-  }, []);
-  */
 export default function Navbar() {
   const [user, setUser] = useState(null);
 
@@ -21,35 +14,43 @@ export default function Navbar() {
     <nav>
       <div className="container-fluid">
         <div className="row align-items-center">
-          <div className="col-lg-1 col-sm-2">
+          <div className="col-2 col-sm-2 col-md-2 col-lg-2">
             <Image name="logo" />
           </div>
 
-          <div className="col-lg-9 col-sm-4 text-start">
+          <div
+            className={
+              user
+                ? "col-6 col-sm-6 col-md-6 col-lg-7 text-start"
+                : "col-4 col-sm-4 col-md-4 col-lg-6 text-start"
+            }
+          >
             <Button link="/home" text="Home" />
           </div>
 
           {user ? (
             <>
-              <div className="col-lg-1 text-end">
+              <div className="col-3 col-sm-3 col-md-3 col-lg-2 text-end">
                 <p className="m-0">{user.name}</p>
               </div>
 
-              <div className="col-lg-1 text-start">
+              <div className="col-1 col-sm-1 col-md-1 col-lg-1 text-start">
                 {user.picture ? (
                   <Link to="/profile">{user.picture}</Link>
                 ) : (
-                  <Link to="/profile"><Icon name="anonymous" /></Link>
+                  <Link to="/profile">
+                    <Icon name="anonymous" />
+                  </Link>
                 )}
               </div>
             </>
           ) : (
             <>
-              <div className="col-lg-1 col-sm-3 text-end">
+              <div className="col-3 col-sm-3 col-md-3 col-lg-2 text-end">
                 <Button link="/signin" text="Sign In" />
               </div>
 
-              <div className="col-lg-1 col-sm-3 text-start">
+              <div className="col-3 col-sm-3 col-md-3 col-lg-2 text-start">
                 <Button link="/signup" text="Sign Up" />
               </div>
             </>
