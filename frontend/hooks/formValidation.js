d6facc96ec53ef550b9183e232924b318d7ee77d
Bibby850 <larandrade116@gmail.com>
2026-03-31 10:43:22 +0100

    refractor: validation
    deleted the useless functions created before

--- frontend/hooks/formValidation.js
+++ frontend/hooks/formValidation.js
@@ -1,8 +1,1 @@
 "use strict";
-import { createArrayById } from "../utils/arrayCreation";
-
-
-export function initializeFormValidation() {
-  const fieldIds = ["username", "email", "password", "passwordVerification"];
-  const fields = createArrayById(fieldIds);
-}
