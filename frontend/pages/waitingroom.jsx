fd6714532267a71234648433220d0297bb35cb23
MineJAG <ajonecas@gmail.com>
2026-04-03 00:08:40 +0100

    feat(waiting-room): add player list and room code display

--- frontend/pages/waitingroom.jsx
+++ frontend/pages/waitingroom.jsx
@@ -1,12 +1,51 @@
 import Navbar from "../components/navbar.jsx";
+import { Image } from "../components/images.jsx";
+import { GameButton } from "../components/button.jsx";
+import { Icon } from "../components/icons.jsx";
 
 import "../styles/variable.css";
 
+import { useState } from "react";
+
 export default function WaitingRoom() {
-    return (
-        <>
-            <Navbar />
-            <h1>Waiting Room</h1>
-        </>
-    );
+  const [roomCode, setRoomCode] = useState("1234"); // Placeholder room code
+
+  const [players, setPlayers] = useState([ // Placeholder player data
+    {
+      name: "Player 1",
+      avatar: null,
+      isHost: true, //placeholder, to be replaced with actual host logic just for testing purposes
+    },
+    {
+      name: "Player 2",
+      avatar: null,
+      isHost: false,
+    },
+  ]);
+
+  return (
+    <div className="waiting-room-page">
+      <Navbar />
+      <div className="container text-center">
+        <div>
+          <h1 className="header">Waiting Room : {roomCode}</h1>
+          <div className="player-list">
+            {players.map((player, index) => (
+              <div key={index} className="player">
+                <div className="player-avatar">
+                  {player.avatar ? (
+                    <Image src={player.avatar}/>
+                  ) : (
+                    <Icon name="anonymous"/>
+                  )}
+                </div>
+                <div className="player-name">{player.name}</div>
+                {player.isHost && <div className="player-host">Host</div>}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
