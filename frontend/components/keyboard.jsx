2485b5da5c13bef36ef7f8524dbd7a4e00f250b7
MineJAG <ajonecas@gmail.com>
2026-04-03 23:32:06 +0100

    feat(keyboard): add keyboard

---
+++ frontend/components/keyboard.jsx
@@ -0,0 +1,29 @@
+import Key from "./key.jsx";
+import "../styles/variable.css";
+import "../styles/keyboard.css";
+
+
+const ROWS = [ //TODO: thinking of making this dynamic in the future 
+  ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "Backspace"],
+  ["Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+  ["CapsLock", "A", "S", "D", "F", "G", "H", "J", "K", "L"],
+  ["Shift", "Z", "X", "C", "V", "B", "N", "M", "Shift"],
+  ["Control", "Alt", "Spacebar", "Alt", "Control"],
+];
+
+export default function Keyboard() {
+
+  return (
+    <div className="kb-wrapper"> 
+      {ROWS.map((row) => (
+        <div className="kb-row">
+          {row.map((letter) => (
+            <Key
+              letter={letter}
+            />
+          ))}
+        </div>
+      ))}d
+    </div>
+  );
+}
