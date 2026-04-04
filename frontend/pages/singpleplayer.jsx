80a75684434020578d2054e7a9974cf3e0f3be9d
MineJAG <ajonecas@gmail.com>
2026-04-04 20:04:11 +0100

    feat: create initial singpleplayer page

---
+++ frontend/pages/singpleplayer.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/navbar.jsx";
+
+import "../styles/variable.css";
+
+export default function Singleplayer() {
+    return (
+        <>
+            <Navbar />
+            <h1>Singleplayer</h1>
+        </>
+    );
+}
