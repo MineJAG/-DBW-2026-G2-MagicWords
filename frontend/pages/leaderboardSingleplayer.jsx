bc052191e8468c7128085993479c40c33ae90104
MineJAG <ajonecas@gmail.com>
2026-04-13 00:55:33 +0100

    feat: create singleplayer leaderboard page

---
+++ frontend/pages/leaderboardSingleplayer.jsx
@@ -0,0 +1,17 @@
+import Navbar from "../components/navbar.jsx";
+
+import "../styles/variable.css";
+
+export default function LeaderboardSingleplayer() {
+  return (
+    <div className="Leaderboard">
+      <Navbar />
+      <div className="container-fluid leaderboard-container">
+        <div className="leaderboard-row">
+          <div className="col-12 col-lg-6 leaderboard-content-col">
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
