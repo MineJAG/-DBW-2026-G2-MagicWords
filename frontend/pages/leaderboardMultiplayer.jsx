3fac082ccf32063debb83e99e74a60bc037b61c9
MineJAG <ajonecas@gmail.com>
2026-04-13 02:54:34 +0100

    feat(leaderboardmultiplayer): add proper content to leaderboard multiplayer page

--- frontend/pages/leaderboardMultiplayer.jsx
+++ frontend/pages/leaderboardMultiplayer.jsx
@@ -1,14 +1,21 @@
 import Navbar from "../components/navbar.jsx";
-
+import Leaderboard from "../components/leaderboard.jsx";
 import "../styles/variable.css";
+import "../styles/leaderboardmultiplayer.css";
 
 export default function LeaderboardMultiplayer() {
   return (
     <div className="Leaderboard">
       <Navbar />
       <div className="container-fluid leaderboard-container">
-        <div className="leaderboard-row">
+        <div className="row justify-content-center">
           <div className="col-12 col-lg-6 leaderboard-content-col">
+            <div className="leaderboard-content">
+            <Leaderboard players = {[]}/>
+            <div className="leaderboard-content-text">
+              Placeholder
+              </div>
+           </div>
           </div>
         </div>
       </div>
