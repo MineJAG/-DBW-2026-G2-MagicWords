48fc6d8151077411a46d01d39b4fb4522c52e647
Bibby850 <larandrade116@gmail.com>
2026-04-15 16:51:42 +0100

    fix: avatar
    now avatar is simpler and separate

---
+++ frontend/hooks/useAvatar.js
@@ -0,0 +1,32 @@
+import { useRef, useState } from "react";
+
+export default function useAvatar(initialSrc = null) {
+  const [picture, setPicture] = useState(initialSrc);
+  const inputRef = useRef(null);
+
+  function handleFileChange(e) {
+    //this is what handles the image change
+    const file = e.target.files[0]; //the selected files are stored in a list so we take the first one
+    if (!file) return; //if no file is selected, return
+
+    const reader = new FileReader(); //file reader is a converter
+
+    reader.onload = (event) => {
+      setPicture(event.target.result);
+    };
+
+    reader.readAsDataURL(file);
+  }
+
+  function openFilePicker() {
+    //this serves to open the folders so u can select the image
+    inputRef.current.click();
+  }
+
+  return {
+    picture,
+    inputRef,
+    handleFileChange,
+    openFilePicker,
+  };
+}
