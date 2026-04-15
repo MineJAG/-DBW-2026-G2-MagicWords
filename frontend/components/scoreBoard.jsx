95655c44d9d9e24e2694ad8e08f27a90aa91f7d0
MineJAG <ajonecas@gmail.com>
2026-04-15 15:28:31 +0100

    feat(scoreboard): restructure layout

--- frontend/components/scoreBoard.jsx
+++ frontend/components/scoreBoard.jsx
@@ -1,30 +1,40 @@
 import { Icon } from "./icons.jsx";
 
-export default function ScoreBoard({players}) {
+export default function ScoreBoard({ players }) {
   const sorted = players.sort((a, b) => (b.score ?? 0) - (a.score ?? 0));
 
   return (
     <div className="scoreboard">
-      <div className="scoreboard-header">
-        <h2>Scoreboard</h2>
+      <div className="row">
+        <div className="col-12">
+          <div className="scoreboard-title">Scoreboard</div>
+        </div>
       </div>
-      <ul className="scoreboard-list">
-        {sorted.map((player, index) => (
-          <li key={player.id ?? index} className="scoreboard-row">
-              <div className="scoreboard-score">{player.score ?? 0}</div>
-              <div className="player-avatar">
-                {player.avatar ? (
-                  <img src={player.avatar}/>
-                ) : (
-                  <Icon name="anonymous" />
-                )}
+      <div className="row">
+        <div className="col-12">
+          <div className="scoreboard-list">
+            {sorted.map((player, index) => (
+              <div key={player.id ?? index} className="row">
+                <div className="col-2">
+                  <div className="scoreboard-score">{player.score ?? 0}</div>
+                </div>
+                <div className="col-3">
+                  <div className="player-avatar">
+                    {player.avatar ? (
+                      <img src={player.avatar} />
+                    ) : (
+                      <Icon name="anonymous" />
+                    )}
+                  </div>
+                </div>
+                <div className="col-7">
+                  <div className="scoreboard-name">{player.name}</div>
+                </div>
               </div>
-              <div className="scoreboard-name">
-                {player.name}
-              </div>
-          </li>
-        ))}
-      </ul>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
