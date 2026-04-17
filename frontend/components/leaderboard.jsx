566ef1c230a707fe6c22188e0306e018f0c3d4f4
MineJAG <ajonecas@gmail.com>
2026-04-17 19:09:58 +0100

    feat(leaderboard): restucture leaderboard

--- frontend/components/leaderboard.jsx
+++ frontend/components/leaderboard.jsx
@@ -1,19 +1,19 @@
 import { AvatarPreview } from "./avatar.jsx";
 import { Icon } from "./icons.jsx";
-import { useGame } from "../context/gameContext.jsx";
 
 import "../styles/variable.css";
 import "../styles/leaderboard.css";
 
 export default function Leaderboard({ players = [] }) {
-  const { players: contextPlayers = [] } = useGame();
-  const activePlayers =
-    Array.isArray(players) && players.length > 0 ? players : contextPlayers;
-
-  const sortedPlayers = [...activePlayers].sort(
+  const sortedPlayers = [...players].sort(
     (firstPlayer, secondPlayer) =>
       (secondPlayer.score ?? 0) - (firstPlayer.score ?? 0)
   );
+
+  const second = sortedPlayers[1] ?? null;
+  const first = sortedPlayers[0] ?? null;
+  const third = sortedPlayers[2] ?? null;
+
   return (
     <div className="Leaderboard-box">
       <div className="row">
@@ -30,14 +30,14 @@ export default function Leaderboard({ players = [] }) {
                   <div className="col-4 leaderboard-box-content-body-left">
                     <div className="leaderboard-box-content-body-left-player">
                       <div className="leaderboard-box-content-body-left-player-avatar">
-                        {sortedPlayers[1]?.avatar ? (
-                          <AvatarPreview src={sortedPlayers[1].avatar} />
+                        {second?.avatar ? (
+                          <AvatarPreview src={second.avatar} />
                         ) : (
                           <Icon name="anonymous" />
                         )}
                       </div>
                       <div className="leaderboard-box-content-body-left-player-name">
-                        {sortedPlayers[1]?.name || "Player 2"}
+                        {second?.name || "Player 2"}
                       </div>
                     </div>
                     <div className="leaderboard-box-content-body-left-graybox">
@@ -47,14 +47,14 @@ export default function Leaderboard({ players = [] }) {
                   <div className="col-4 leaderboard-box-content-body-center">
                     <div className="leaderboard-box-content-body-center-player">
                       <div className="leaderboard-box-content-body-center-player-avatar">
-                        {sortedPlayers[0].avatar ? (
-                          <AvatarPreview src={sortedPlayers[0].avatar} />
+                        {first.avatar ? (
+                          <AvatarPreview src={first.avatar} />
                         ) : (
                           <Icon name="anonymous" />
                         )}
                       </div>
                       <div className="leaderboard-box-content-body-center-player-name">
-                        {sortedPlayers[0].name || "Player 1"}
+                        {first.name || "Player 1"}
                       </div>
                     </div>
                     <div className="leaderboard-box-content-body-center-yellowbox">
@@ -64,14 +64,14 @@ export default function Leaderboard({ players = [] }) {
                   <div className="col-4 leaderboard-box-content-body-right">
                     <div className="leaderboard-box-content-body-right-player">
                       <div className="leaderboard-box-content-body-right-player-avatar">
-                        {sortedPlayers[2].avatar ? (
-                          <AvatarPreview src={sortedPlayers[2].avatar} />
+                        {third.avatar ? (
+                          <AvatarPreview src={third.avatar} />
                         ) : (
                           <Icon name="anonymous" />
                         )}
                       </div>
                       <div className="leaderboard-box-content-body-right-player-name">
-                        {sortedPlayers[2].name || "Player 3"}
+                        {third.name || "Player 3"}
                       </div>
                     </div>
                     <div className="leaderboard-box-content-body-right-brownbox">
