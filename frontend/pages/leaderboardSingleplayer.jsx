ad497081ee80b6359f75a71d24c6e33563a5c830
MineJAG <ajonecas@gmail.com>
2026-04-17 19:41:27 +0100

    feat(leaderboardsingleplayer): add context logic

--- frontend/pages/leaderboardSingleplayer.jsx
+++ frontend/pages/leaderboardSingleplayer.jsx
@@ -1,52 +1,59 @@
 import { useUser } from "../context/userContext.jsx";
+import { useGame } from "../context/gameContext.jsx";
 
 import Navbar from "../components/navbar.jsx";
-import { useState } from "react";
-import  { AvatarPreview } from "../components/avatar.jsx";
+import { AvatarPreview } from "../components/avatar.jsx";
 import { Icon } from "../components/icons.jsx";
 
 import "../styles/variable.css";
 import "../styles/leaderboardSingleplayer.css";
 
 export default function LeaderboardSinglePlayer() {
-      const { user } = useUser();
+  const { user } = useUser();
+  const { players } = useGame();
+  const activePlayer = players.find((player) => player.isHost) ?? players[0];
+  const currentScore = activePlayer?.score ?? user?.stats?.currentScore ?? 0;
+  const userStats = user?.stats ?? {};
+
   return (
     <div className="Leaderboard">
       <Navbar />
       <div className="container-fluid leaderboard-container">
         <div className="row">
-            <div className="col-6 leaderboard-content-score">
-                <div className="row">
-                <div className="col-12">
-                    <div className="leaderboard-content-score-value">
-                        Score: {user?.stats.currentScore}  
-                    </div>
-                    <div className="leaderboard-content-score-yellowbox"></div>
+          <div className="col-6 leaderboard-content-score">
+            <div className="row">
+              <div className="col-12">
+                <div className="leaderboard-content-score-value">
+                  Score: {currentScore}
                 </div>
-                </div>
+                <div className="leaderboard-content-score-yellowbox"></div>
+              </div>
             </div>
-            <div className="col-6 leaderboard-content-stats">
-                <div className="leaderboard-content-stats-title"> LeaderBoard-Singleplayer</div>
-                <div className="row">
-                    <div className="col-6 leaderboard-content-stats-user-Avatar">
-                        {user?.picture ? (
-                            <AvatarPreview src={user?.picture}/>
-                        ) : (
-                            <Icon name="anonymous" />
-                        )}
-                    </div>
-                    <div className="col-6 leaderboard-content-stats-user-name">
-                        {user?.name}
-                    </div>
-                </div>
-                <div className="leaderboard-content-stats-list">
-                {Object.entries(user?.stats).map(([key, value]) => (
-                    <div key={key} className="leaderboard-content-stats-item">
-                        <strong>{key.replace(/([A-Z])/g, ' $1')}:</strong> {value}
-                    </div>
-                ))}
-                </div>
+          </div>
+          <div className="col-6 leaderboard-content-stats">
+            <div className="leaderboard-content-stats-title">
+              LeaderBoard-Singleplayer
             </div>
+            <div className="row">
+              <div className="col-6 leaderboard-content-stats-user-Avatar">
+                {user?.picture ? (
+                  <AvatarPreview src={user?.picture} />
+                ) : (
+                  <Icon name="anonymous" />
+                )}
+              </div>
+              <div className="col-6 leaderboard-content-stats-user-name">
+                {user?.name}
+              </div>
+            </div>
+            <div className="leaderboard-content-stats-list">
+              {Object.entries(userStats).map(([key, value]) => (
+                <div key={key} className="leaderboard-content-stats-item">
+                  <strong>{key.replace(/([A-Z])/g, " $1")}:</strong> {value}
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
