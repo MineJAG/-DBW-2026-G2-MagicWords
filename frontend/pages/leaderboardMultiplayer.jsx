6910f5b6adbdb8827c77484c980fa5b55fdb9780
MineJAG <ajonecas@gmail.com>
2026-04-17 19:08:32 +0100

    feat(leaderboardmultiplayer): add proper context

--- frontend/pages/leaderboardMultiplayer.jsx
+++ frontend/pages/leaderboardMultiplayer.jsx
@@ -2,8 +2,14 @@ import Navbar from "../components/navbar.jsx";
 import Leaderboard from "../components/leaderboard.jsx";
 import "../styles/variable.css";
 import "../styles/leaderboardmultiplayer.css";
+import { useGame } from "../context/gameContext.jsx";
+
 
 export default function LeaderboardMultiplayer() {
+  const {
+    players,
+  } = useGame();
+
   return (
     <div className="Leaderboard">
       <Navbar />
@@ -11,7 +17,7 @@ export default function LeaderboardMultiplayer() {
         <div className="row justify-content-center">
           <div className="col-12 col-lg-6 leaderboard-content-col">
             <div className="leaderboard-content">
-              <Leaderboard />
+              <Leaderboard players={players} />
             </div>
           </div>
         </div>
