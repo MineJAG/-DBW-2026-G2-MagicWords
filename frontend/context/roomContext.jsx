afac8dcf29f68330cf5121899b12675b96cd2712
MineJAG <ajonecas@gmail.com>
2026-04-17 15:21:02 +0100

    feat(roomContext): add isHost parameter to playeres

--- frontend/context/roomContext.jsx
+++ frontend/context/roomContext.jsx
@@ -5,12 +5,12 @@ const RoomContext = createContext(null);
 export function RoomProvider({ children }) {
   const [room, setRoom] = useState(1234);
   const [roomPlayers, setRoomPlayers] = useState([
-    { id: 1, name: "player1", score: 1234, avatar: null },
-    { id: 2, name: "player2", score: 1180, avatar: null },
-    { id: 3, name: "player3", score: 1102, avatar: null },
-    { id: 4, name: "player4", score: 945, avatar: null },
-    { id: 5, name: "player5", score: 880, avatar: null },
-    { id: 6, name: "player6", score: 810, avatar: null },
+    { id: 1, name: "player1", score: 1234, avatar: null, isHost: true },
+    { id: 2, name: "player2", score: 2180, avatar: null, isHost: false },
+    { id: 3, name: "player3", score: 1102, avatar: null, isHost: false },
+    { id: 4, name: "player4", score: 945, avatar: null, isHost: false },
+    { id: 5, name: "player5", score: 880, avatar: null, isHost: false },
+    { id: 6, name: "player6", score: 810, avatar: null, isHost: false },
   ]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
