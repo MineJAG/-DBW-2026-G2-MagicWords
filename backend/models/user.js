5faa82744a34cd92701d2406f47f1b40001b5289
Bibby850 <larandrade116@gmail.com>
2026-05-02 12:15:10 +0100

    fix: user model
    it had the incorrect stats

--- backend/models/user.js
+++ backend/models/user.js
@@ -13,15 +13,15 @@ const userSchema = new mongoose.Schema(
       gamesPlayed: { type: Number, default: 0 },
       gamesWon: { type: Number, default: 0 },
       gamesLost: { type: Number, default: 0 },
+      winRate: { type: Number, default: 0 },
+
       totalWordsFound: { type: Number, default: 0 },
-      totalWordsWrong: { type: Number, default: 0 },
-      totalWordLetters: { type: Number, default: 0 },
-      totalPlayTimeSeconds: { type: Number, default: 0 },
+      averageWordLength: { type: Number, default: 0 },
+      longestWordFound: { type: String, default: null },
 
       highestScore: { type: Number, default: 0 },
       mostWordsInOneMatch: { type: Number, default: 0 },
-      longestWordFound: { type: String, default: null },
-      longestCorrectStreak: { type: Number, default: 0 },
+      longestStreak: { type: Number, default: 0 },
     },
   },
   { timestamps: true }
